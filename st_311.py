bc13e689b0d8762833b0de8d76c4bbb7b7a3df9e
benarnav <117926567+benarnav@users.noreply.github.com>
2023-03-22 21:15:48 -0400

    Update st_311.py

--- st_311.py
+++ st_311.py
@@ -6,7 +6,7 @@ import pandas as pd
 st.set_page_config(page_title="NYC 311 Bike Lane Complaints since Oct 2016", layout="wide")
 
 
-client = Socrata("data.cityofnewyork.us", db_token, timeout=100000000)
+client = Socrata("data.cityofnewyork.us", st.secrets.db_token, timeout=100000000)
 results = client.get("erm2-nwe9", complaint_type="Illegal Parking", status="Closed", descriptor='Blocked Bike Lane', limit=2500000)
 df = pd.DataFrame.from_records(results)
 
